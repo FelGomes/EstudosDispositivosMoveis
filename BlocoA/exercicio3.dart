9e0586d55b31f8d23dc907bb927355f4d49b92cf
Felipe Gomes <felipeferreiraag0@gmail.com>
2026-08-15 15:43:41 -0300

    Adcionando exercicio 3

---
+++ BlocoA/exercicio3.dart
@@ -0,0 +1,22 @@
+// Declare uma constante para o alqueire goiano (4,84 ha) e outra variável com final
+// recebendo DateTime.now(). Tente trocar o final por const, leia o erro e explique a
+// diferença entre os dois
+
+
+const alqueire = 4.84;
+const dataAtual = DateTime.now();
+
+void main() {
+  print(alqueire);
+  print(dataAtual);
+}
+
+
+// Error: Cannot invoke a non-'const' constructor where a const expression is expected.
+// Try using a constructor or factory that is 'const'.
+
+/**
+Nada mais é que nao se pode usar um valor constante para uma variavel que recebe um valor dinamico
+Por hora ser um valor mutavel, nao é possivel
+O valor da variavel sempre vai ser diferente ao rodar
+*/

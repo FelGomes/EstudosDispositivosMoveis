1aba7eff625a02b5e6ae25a851c357e864838f8e
Felipe Gomes <felipeferreiraag0@gmail.com>
2026-08-16 10:04:35 -0300

    Exercicio 1 de Segurança contra nulo

---
+++ BlocoB/exercicio1.dart
@@ -0,0 +1,9 @@
+// Declare String? telefone sem valor inicial e imprima-a usando ?? para exibir "não
+// informado" quando estiver vazia
+
+String? telefone;
+
+
+void main() {
+  print(telefone ?? "Nao informado");
+}

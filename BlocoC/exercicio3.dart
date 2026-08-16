e0f7f431459dcceb11843a20e1d1f54f3116b901
Felipe Gomes <felipeferreiraag0@gmail.com>
2026-08-16 12:50:54 -0300

    Imprimindo uma frase cheia de variavel

---
+++ BlocoC/exercicio3.dart
@@ -0,0 +1,21 @@
+// Crie um texto de várias linhas com o endereço completo de uma cooperativa e imprima-o.
+
+
+
+void main(){
+
+
+String rua = "Rua 18";
+String bairro = "Jaridm sorriso II";
+String numero = "S/N";
+String cooperativa = "Cooperativa Industrial do Centro Oeste de produção de graos";
+String cidade = "Ceres";
+String estado = "Goias";
+String uf = "GO";
+
+
+print("Rua $rua, Bairro: $bairro, Numero: $numero, Cidade: $cidade, Estado: $estado, UF: $uf, Cooperativa: $cooperativa");
+
+
+
+}

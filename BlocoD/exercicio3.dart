e1e3b25e498d5c67a72ad24442af755a4aad4790
Felipe Gomes <felipeferreiraag0@gmail.com>
2026-08-18 16:29:22 -0300

    Hectares para alqueiresgoiano

---
+++ BlocoD/exercicio3.dart
@@ -0,0 +1,26 @@
+// Imprima uma tabela de conversão de hectares para alqueires goianos, de 10 em 10, de 10
+// até 100 hectares.
+
+String? calcularHectaresParaAlqueires(double alqueiresGoiano){
+
+
+
+  for(int ha = 0; ha <= 100; ha += 10){
+    double alqueires = ha/alqueiresGoiano;
+
+    print("${ha} hectares equivale a ${alqueires.toStringAsFixed(2)}");
+  }
+
+
+
+}
+
+
+void main() {
+
+const double alqueiresGoiano = 4.84;
+
+print(calcularHectaresParaAlqueires(alqueiresGoiano));
+
+
+}
